af213a04166cd6e2057f119a3e737216ecfc245a
Nadawoo <nadatruc@gmail.com>
2020-04-14 01:15:01 +0200

    Responsive styles moved to the general CSS

--- view/HtmlPage.php
+++ view/HtmlPage.php
@@ -27,8 +27,8 @@ class HtmlPage
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
                 <link rel="stylesheet" type="text/css" href="resources/css/sitelayout.css?v3.3">
-                <link rel="stylesheet" type="text/css" href="resources/css/gamelayout.css?v3.5">
-                <link rel="stylesheet" type="text/css" href="resources/css/map.css?v1.9">
+                <link rel="stylesheet" type="text/css" href="resources/css/gamelayout.css?v3.6">
+                <link rel="stylesheet" type="text/css" href="resources/css/map.css?v2.0">
                 <link rel="stylesheet" type="text/css" href="resources/css/smartphone.css?v1.2">
                 <link rel="stylesheet" type="text/css" href="resources/css/myzone.css?v1.2">
                 <link rel="stylesheet" type="text/css" href="resources/css/city.css?v1.61">
