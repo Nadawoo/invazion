a4fe0a4bbcbcbb86baf20a1620771454ab483f60
Nadawoo <nadatruc@gmail.com>
2020-06-05 00:38:55 +0200

    Revert "ZombLib : common method to update cookies"
    This reverts commit 14b34e9d5cd0038c9d9140efc3c6ef6eb4adaa6c.

--- ZombLib.php
+++ ZombLib.php
@@ -4,7 +4,7 @@
  * pour récupérer ou/et écrire des données sur le serveur
  * http://invazion.nadazone.fr
  * 
- * Version 4.1
+ * Version 4
  */
 class ZombLib
 {
@@ -105,7 +105,10 @@ class ZombLib
         // Si la connexion a réussi
         if ($api_results['metas']['error_code'] === 'success') {            
             // On stocke le jeton d'identification dans un cookie
-            $this->update_cookie('token', $api_results['datas']['token']);
+            setcookie('token', $api_results['datas']['token']);
+            // On crée aussi la variable, sinon le cookie n'existerait pas 
+            // tant que le script en cours ne s'est pas terminé
+            $_COOKIE['token'] = $api_results['datas']['token'];
         }
         
         return $api_results;
@@ -125,7 +128,10 @@ class ZombLib
         
         if ($api_results['metas']['error_code'] === 'success') {
             // Détruit le cookie d'identification dans le navigateur du joueur
-            $this->update_cookie('token', NULL);
+            setcookie('token', NULL);
+            // On détruit aussi la variable, sinon le cookie survivrait jusqu'à
+            // la fin de l'exécution du script
+            unset($_COOKIE['token']);
         }
         
         return $api_results;
@@ -145,7 +151,10 @@ class ZombLib
         
         if ($result['metas']['error_code'] === 'success') {            
             // On met à jour le jeton dans le cookie
-            $this->update_cookie('token', $result['datas']['new_token']);
+            setcookie('token', $result['datas']['new_token']);
+            // On modifie aussi la variable, sinon le cookie n'existerait pas 
+            // tant que le script en cours ne s'est pas terminé
+            $_COOKIE['token'] = $result['datas']['new_token'];
         }
         
         return $result;
@@ -580,23 +589,6 @@ class ZombLib
         // car chez certains hébergeurs (notamment OVH), cette fonction 
         // ne fonctionne pas avec la variable $_SERVER
         return filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP);
-    }
-    
-    
-    /**
-     * Creates or updates a cookie
-     * 
-     * @param string $name  The name of the cookie
-     * @param string $value The content of the cookie
-     */
-    private function update_cookie($name, $value)
-    {
-        
-        // Sets or updates the value of the cookie in the browser
-        setcookie($name, $value);
-        // Updates the PHP variable too, otherwise the new value of the cookie
-        // would be ignored until the script has finished running.
-        $_COOKIE[$name] = $value;
     }
     
     
