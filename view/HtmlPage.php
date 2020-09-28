3ebe332d5ededf50406228c253d436c4525b2b65
Nadawoo <nadatruc@gmail.com>
2020-09-28 16:07:42 +0200

    [Minor] Don't select text on the map zones (ugly)

--- view/HtmlPage.php
+++ view/HtmlPage.php
@@ -42,7 +42,7 @@ class HtmlPage
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
                 <link rel="stylesheet" type="text/css" href="resources/css/sitelayout.css?v3.8">
                 <link rel="stylesheet" type="text/css" href="resources/css/gamelayout.css?v6.0">
-                <link rel="stylesheet" type="text/css" href="resources/css/map.css?v2.7">
+                <link rel="stylesheet" type="text/css" href="resources/css/map.css?v2.8">
                 <link rel="stylesheet" type="text/css" href="resources/css/smartphone.css?v1.5">
                 <link rel="stylesheet" type="text/css" href="resources/css/myzone.css?v1.4">
                 <link rel="stylesheet" type="text/css" href="resources/css/city.css?v2.8">
