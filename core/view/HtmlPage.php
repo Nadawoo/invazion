b4228e02f55fa54df4beda42dc753123ecb58cd4
Nadawoo <nadatruc@gmail.com>
2022-10-26 19:03:05 +0200

    [Minor] Fix 2 CSS warnings

--- core/view/HtmlPage.php
+++ core/view/HtmlPage.php
@@ -83,7 +83,7 @@ class HtmlPage
                 <link rel="stylesheet" type="text/css" href="resources/css/smartphone.css?v1.6">
                 <link rel="stylesheet" type="text/css" href="resources/css/myzone.css?v1.4">
                 <link rel="stylesheet" type="text/css" href="resources/css/city.css?v3.5">
-                <link rel="stylesheet" type="text/css" href="resources/css/cityiso.css?v1.1">
+                <link rel="stylesheet" type="text/css" href="resources/css/cityiso.css?v1.2">
                 <link rel="stylesheet" type="text/css" href="resources/css/wall.css?v3.1">
                 <link rel="stylesheet" type="text/css" href="resources/css/popup.css?v2.0">
                 <link rel="stylesheet" type="text/css" href="resources/css/responsive.css?v2.4">
