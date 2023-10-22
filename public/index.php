59a5bdaa35896b509b5d2e047cec62491599148a
Nadawoo <nadatruc@gmail.com>
2023-10-22 15:52:09 +0200

    The current day id now given by the "maps" API

--- public/index.php
+++ public/index.php
@@ -156,7 +156,7 @@ $html = [
     // Assembling the HTML for the map
     'map' => $map->hexagonal_map($maps['map_width'], $maps['map_height'], $maps['zones'], $citizen, $maps['next_attack_hour']),
     'map_citizens'      => $layout->map_citizens($citizens),
-    'attack_bar'        => $layout->attack_bar($citizen['map_id'], get_game_day($citizen['last_death'])),
+    'attack_bar'        => $layout->attack_bar($citizen['map_id'], $configs['map']['current_cycle']),
     // Contents of the round action buttons at the right of the map
     'ground_items'      => $layout->block_ground_items($citizen['coord_x'], $citizen['coord_y']),
     // TODO: merge_zone_items with ground_items
