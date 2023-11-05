9808b724738781d7b97fcd675a3b1f8290b5698e
Nadawoo <nadatruc@gmail.com>
2023-11-05 21:01:51 +0100

    [Fix] Let enough height for the "connection" block when not logged in

--- core/view/HtmlPage.php
+++ core/view/HtmlPage.php
@@ -11,7 +11,7 @@ class HtmlPage
     // Increment those variables when you modify the CSS or JS files. This ensures
     // that the users' browsers reload the up-to-date files, instead of using 
     // the obsolete ones stored in their cache.
-    private $css_version = 20;
+    private $css_version = 20.1;
     private $js_version  = 20;
     
     /**
