880fb9767861315a819a53a7a2c4aba53ad9102a
Nadawoo <nadatruc@gmail.com>
2024-02-26 23:10:12 +0100

    Constructions list: let enough height for 2 lines of components

--- core/view/HtmlPage.php
+++ core/view/HtmlPage.php
@@ -11,7 +11,7 @@ class HtmlPage
     // Increment those variables when you modify the CSS or JS files. This ensures
     // that the users' browsers reload the up-to-date files, instead of using 
     // the obsolete ones stored in their cache.
-    private $css_version = 33.2;
+    private $css_version = 33.3;
     private $js_version  = 33;
     
     /**
