c5dd632eec9fd3608b84b91692d0cc467b981bde
Nadawoo <nadatruc@gmail.com>
2024-03-21 12:18:27 +0100

    [Minor] Fix color of the link "See other messages" in the game events

--- public/resources/js/wallTemplate.js
+++ public/resources/js/wallTemplate.js
@@ -20,7 +20,7 @@ function htmlDiscussionNotif(topicTitle, date, url, authorId, authorPseudo, last
 
 function htmlDiscussion(topicId, topicType, nbrReplies) {
     
-    var otherMessagesLink = (nbrReplies>1) ? '<a class="link_other_messages" onclick="loadDiscussion('+topicId+')">└ &nbsp;voir les '+(nbrReplies-1)+' autres réponses&nbsp; ┐</a>' : '';
+    var otherMessagesLink = (nbrReplies>1) ? '<a class="link_other_messages" onclick="loadDiscussion('+topicId+')"><span>└</span>voir les '+(nbrReplies-1)+' autres réponses<span>┐</span></a>' : '';
     
     return '<div id="topic'+topicId+'" class="topic '+topicType+'">\
                 <h3>\
