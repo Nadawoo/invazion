ea8db64ea7f3c7061c55aaa661fe7adfc59585ad
Nadawoo <nadatruc@gmail.com>
2024-11-12 22:36:15 +0100

    [JS] Move user-related functions to users.func.js

--- public/resources/js/misc.func.js
+++ public/resources/js/misc.func.js
@@ -249,16 +249,6 @@ function toggleItemsPanel() {
         setCookie('showitemspanel', 0);
     }
 }
-
-
-/**
- * Gets the ID of the connected player
- * @returns {int}
- */
-function getCitizenId() {
-
-    return parseInt(document.getElementById("citizenId").innerHTML);
-}
     
 
 /*
@@ -294,43 +284,6 @@ function activatePhoneTab(tabId=null) {
     hide(["#minimap", "#health", "#zone"]);
     // Puis on affiche le contenu de l'onglet actif
     display(`#${tabId}`);
-}
-
-
-/**
- * Connects the user to his account (sends logins and gets the API result)
- */
-async function connectUser() {
-    
-    let emailField  = document.getElementById("email"),
-        email       = emailField.value,
-        password    = document.getElementById("password").value;
-    
-    if (email === '') {
-        
-        document.getElementById("error").innerHTML = "Veuillez indiquer l'adresse mail que vous aviez utilisée "
-                        +"lors de votre inscription.<br>"
-                        +"<em>Pas encore inscrit ? <a href=\"register.php\">Créez votre compte maintenant !</a></em>";
-    }
-    else if (emailField.checkValidity() !== true) {
-        
-        document.getElementById("error").innerHTML = "L'email que vous avez saisi est invalide. Vérifiez qu'il ne contient pas une faute de frappe...";
-    }
-    else {
-        // Calls the connection API
-        let json = await callApi("POST", "user", `action=connect&email=${email}&password=${password}`);
-
-        if (json.metas.error_code !== "success") {
-
-            document.getElementById("error").innerHTML = json.metas.error_message;
-        }
-        else {
-            // Stores the identification token in a cookie
-            setCookie("token", json.datas.token);
-            // Redirects to the main game page after the connction
-            window.location.replace("index.php#Outside");
-        }
-    }
 }
 
 
--- public/resources/js/users.func.js
+++ public/resources/js/users.func.js
@@ -5,6 +5,43 @@
 
 
 /**
+ * Connects the user to his account (sends logins and gets the API result)
+ */
+async function connectUser() {
+    
+    let emailField  = document.getElementById("email"),
+        email       = emailField.value,
+        password    = document.getElementById("password").value;
+    
+    if (email === '') {
+        
+        document.getElementById("error").innerHTML = "Veuillez indiquer l'adresse mail que vous aviez utilisée "
+                        +"lors de votre inscription.<br>"
+                        +"<em>Pas encore inscrit ? <a href=\"register.php\">Créez votre compte maintenant !</a></em>";
+    }
+    else if (emailField.checkValidity() !== true) {
+        
+        document.getElementById("error").innerHTML = "L'email que vous avez saisi est invalide. Vérifiez qu'il ne contient pas une faute de frappe...";
+    }
+    else {
+        // Calls the connection API
+        let json = await callApi("POST", "user", `action=connect&email=${email}&password=${password}`);
+
+        if (json.metas.error_code !== "success") {
+
+            document.getElementById("error").innerHTML = json.metas.error_message;
+        }
+        else {
+            // Stores the identification token in a cookie
+            setCookie("token", json.datas.token);
+            // Redirects to the main game page after the connction
+            window.location.replace("index.php#Outside");
+        }
+    }
+}
+
+
+/**
  * Check if the user (connected or not) has a citizen in the current game (map)
  * 
  * @returns {Boolean}
@@ -12,4 +49,14 @@
 function isCitizenInGame() {
     
     return Number.isInteger(parseInt(document.querySelector("#citizenId").innerText));
+}
+
+
+/**
+ * Gets the ID of the connected player
+ * @returns {int}
+ */
+function getCitizenId() {
+
+    return parseInt(document.getElementById("citizenId").innerHTML);
 }
