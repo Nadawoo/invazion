3be9653687cbfeef1384a982d6418fa413793f08
Nadawoo <nadatruc@gmail.com>
2025-01-05 20:07:39 +0100

    Add animations for the entrances of the action cards

--- core/view/HtmlActionCards.php
+++ core/view/HtmlActionCards.php
@@ -17,7 +17,7 @@ class HtmlActionCards {
     function card_citizens() {
         
         return '
-            <a id="card_citizens" class="card"
+            <a id="card_citizens" class="card animate__animated animate__slideInLeft"
                 onclick="toggleActionBlock(\'citizens\'); updateBlockAction(\'citizens\')">
                 D\'autres humains se trouvent dans la zone !
                 <strong>&#x1F465; Interagir &#9002;</strong>
@@ -28,7 +28,7 @@ class HtmlActionCards {
     function card_building() {
         
         return '
-            <div id="card_building" class="card">
+            <div id="card_building" class="card animate__animated animate__slideInDown">
                 <img src="resources/img/copyrighted/tiles/desert/10.png" height="96" width="73" alt="Bâtiment">
                 <div style="margin-left:5.5rem">
                     <strong>Bâtiment découvert :<br><span class="building_name"></span></strong>
@@ -46,7 +46,7 @@ class HtmlActionCards {
     function card_dig() {
         
         return '
-            <a id="card_dig" class="card"
+            <a id="card_dig" class="card animate__animated animate__slideInLeft"
                 onclick="toggleActionBlock(\'dig\'); updateBlockAction(\'dig\')">
                 La zone peut être fouillée.
                 <strong>&#9935;&#65039; Fouiller &#9002;</strong>
@@ -57,7 +57,7 @@ class HtmlActionCards {
     function card_ap_cost() {
         
         return '
-            <a href="#popmove" id="card_ap_cost" class="card">
+            <a href="#popmove" id="card_ap_cost" class="card animate__animated animate__slideInLeft">
                 <span>Quitter la zone vous coûtera <strong>1</strong>&#9889;</span>
                 <span class="actionspoints_decrease"></span>
              </a>';
