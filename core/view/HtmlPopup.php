4808ee51383b6e1528725a871cb62eb35d236b53
Nadawoo <nadatruc@gmail.com>
2026-05-19 20:22:27 +0200

    Remove the old button for starting a game (replaced by the /games page)

--- core/view/HtmlPopup.php
+++ core/view/HtmlPopup.php
@@ -361,8 +361,7 @@ class HtmlPopup
             $msg_popup .= '<p>[Bug: motif de mort inconnu]</p>';
         }
         
-        $msg_popup .= $buttons->button('start_game', true, 'center')
-                    . $buttons->button('validate_death', true, 'center');
+        $msg_popup .= $buttons->button('validate_death', true, 'center');
         
         return $msg_popup;
     }
