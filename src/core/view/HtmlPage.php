6c6c2e54cebab19789ebf976dc1f06518c2fb17f
Nadawoo <nadatruc@gmail.com>
2026-07-31 14:41:16 +0200

    [Minor] Radial menu: move the inline-styles to CSS

--- src/core/view/HtmlPage.php
+++ src/core/view/HtmlPage.php
@@ -11,7 +11,7 @@ class HtmlPage
     // Increment those variables when you modify the CSS or JS files. This ensures
     // that the users' browsers reload the up-to-date files, instead of using 
     // the obsolete ones stored in their cache.
-    private $css_js_version = 229;
+    private $css_js_version = 230;
     
     /**
      * Sets HTTP headers to secure the website
